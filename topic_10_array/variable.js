5831f36e0ed90a8007e1bd3f97510c40f58ce7bc
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-02 06:30:45 +0600

    first commit

---
+++ topic_10_array/variable.js
@@ -0,0 +1,10 @@
+// Array
+var public1 = 50;
+var public2 = 100;
+var public3 = 300;
+var public4 = 400;
+var public5 = 500;
+
+var publics = [50, 100, 300, 400, 500];
+var friendsAge = [15, 18, 22, 11, 30];
+var friendsName = ['Rakib', "Rifat", "Shamim", 'Farabi']
---
+++ topic_9_boolean/variable.js
@@ -0,0 +1,16 @@
+console.log(3>5);
+console.log(5>2);
+console.log(5<10);
+
+
+// Step by Step line by line Read
+var x = "Shariar Ahamed";
+x = "Shakib Khan";
+x = "Amir Khan";
+console.log(x);
+
+
+var y = 10;
+y = 20;
+y = 30;
+console.log(y)
