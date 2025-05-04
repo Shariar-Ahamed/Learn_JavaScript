64d31085892fecb2a59bae9f53b8b1f413dfedcf
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-04 10:14:53 +0600

    first commit

---
+++ topic_14_unshift_shift_array/variable.js
@@ -0,0 +1,36 @@
+/**** Add value to the beginning of an array using unshift() method ****/
+
+// unshift() (Add to the fist of the array)
+var x = [4, 2, 1, 5, 7, 9, 3];
+x.unshift(11);
+x.unshift(22);
+
+console.log(x);
+
+
+// unshift() (Add to the fist of the array)
+var name = ['Rakib', "Rifat", "Shamim", 'Farabi'];
+name.unshift("Arif");
+name.unshift("Abdul");
+
+console.log(name);
+
+
+
+
+/**** Remove the first element of an array using shift() method ****/ 
+
+// shift() (Remove from the first of the array)
+var x = [4, 2, 1, 5, 7, 9, 3];
+x.shift();
+x.shift();
+
+console.log(x);
+
+
+// shift() (Remove from the first of the array)
+var name = ['Rakib', "Rifat", "Shamim", 'Farabi'];
+name.shift();
+name.shift();
+
+console.log(name)
