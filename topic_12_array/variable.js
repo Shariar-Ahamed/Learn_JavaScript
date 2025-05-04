9be6e53b6c03d41d10aa0523c398ede39a3749a0
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-04 08:34:48 +0600

    first commit

---
+++ topic_12_array/variable.js
@@ -0,0 +1,24 @@
+// Array
+
+var number = [50, 80, 77, 45, 90, 100 ];
+console.log(number.length);
+
+
+
+console.log(number.indexOf(77));
+
+var element = number.indexOf(77);
+console.log(element);
+
+
+
+console.log(number[2]);
+
+var secndElement = number[2];
+console.log(secndElement);
+
+
+
+console.log(number);
+number[0] = 33;
+console.log(number);
