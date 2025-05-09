4bf935a1aabfce42d0a19400c2e28641ac39de43
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-09 09:19:24 +0600

    update

---
+++ topic_18_Primitive_&_NonPrimitive/index.js
@@ -0,0 +1,19 @@
+// Primitive Data Types
+var person = "Shariar Ahamed";
+var newPerson =  person;
+person = "Rakib Hassan";
+
+console.log(person); // Rakib Hassan
+console.log(newPerson); // Shariar Ahamed
+
+
+
+
+
+// Non-Primitive Data Types
+var person1 = {name: "Shamim Reza"};
+var newPerson1 =  person1;
+person1.name = "Arif Hossain";
+
+console.log(person1); //{ name: 'Arif Hossain' }
+console.log(newPerson1); //{ name: 'Arif Hossain' }
---
+++ topic_4_data_type/topic.js
@@ -0,0 +1,46 @@
+
+/**  Math operations and data types in JavaScript
+
+   1. Number: 10, 5, 20, 68, 70;
+   2. Integer:  3, 5, 10, 48, 69;
+   3. Float: Decimal: 10.23, 5.6, 48.5, 98.99;
+   4. -5, 5, -1, -2, -100, 1, 2, 100;
+   5. Operation: Addition +, Subtraction -, Multiplication *, Division / ;
+   6. Percent %, 
+   7. Average 10, 50, 6, 68, 90 ;
+   8. Remainder/Modulus  4/3 = .1 ;
+   9. Comparison: 10, 10, 50, 6, 68, 90 ;
+   10. >, <, ==, === ;
+
+
+   1. Number
+   2. String
+   3. Boolean
+   4. Array
+   5. Object (Undefined)
+   6. Undefined (Undefined)
+   7. Null (Undefined)
+
+   8. Primitive Data 
+      *Number
+      *String
+      *Boolean
+      *Null
+      *Undefined
+      *Symbol (ES6)
+      *BigInt (ES11)
+
+   9. Non-Primitive Data / Reference Data Type
+      *Object
+      *Array
+      *Function
+      *Date
+      *RegExp
+      *Error
+   
+```````````````````````````````````````````````````````
+   
+   
+
+   
+*/
