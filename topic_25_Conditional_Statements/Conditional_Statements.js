70e68cf4557a088c86babe4dafd4150c2dc82543
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-11 07:37:46 +0600

    Update

---
+++ topic_25_Conditional_Statements/Conditional_Statements.js
@@ -0,0 +1,24 @@
+let burgerPrice = 90;
+let myBudget = 70;
+
+if (burgerPrice <= myBudget) {
+    console.log("I can buy the burger"); 
+}
+
+
+let eggPrice = 50;
+let myMoney = 70;
+
+if (eggPrice < myMoney) {
+    console.log("I can buy the egg"); // 'I can buy the egg'
+}
+
+
+let appplePrice = 100;
+let myCash = 70;
+
+if (myCash >= appplePrice) {
+    console.log("I can buy the apple");
+} else {
+    console.log("I can not buy the apple"); // 'I can not buy the apple'
+}
---
+++ topic_26_If_Else_Statements/If_Else_Statements.js
@@ -0,0 +1,22 @@
+
+let score = 50;
+
+
+if (score >= 90) {
+    console.log("Grade: A+");
+}
+else if (score >= 80) {
+    console.log("Grade: A");
+}
+else if (score >= 70) {
+    console.log("Grade: B");
+}
+else if (score >= 60) {
+    console.log("Grade: C");
+}
+else if (score >= 50) {
+    console.log("Grade: D"); // 'Grade: D'
+}
+else {
+    console.log("Grade: F");
+}
