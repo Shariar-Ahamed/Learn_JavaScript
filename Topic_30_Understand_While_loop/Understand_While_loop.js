970d58ec6f20064fc038c3c0d869df3d39ef657b
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-13 08:13:34 +0600

    Update

--- Topic_30_Understand_While_loop/Understand_While_loop.js
+++ Topic_30_Understand_While_loop/Understand_While_loop.js
@@ -1,8 +1,20 @@
-// while loop
+// Understand_While_loop
 
 let count = 10;
 
-while (count > 0) {
-    console.log(count); // 10 9 8 7 6 5 4 3 2 1
+while (count >= 0) {
+    console.log(count); // 10 9 8 7 6 5 4 3 2 1 0
     count--;
+}
+
+
+
+
+// while loop with break statement
+const myArray = ["Shahriar", "Shuvo", "Shanto", "Shamim", "Shakib"];
+let counts = 0;
+
+while (counts < myArray.length) {
+      console.log(myArray[counts]); // Shahriar Shuvo Shanto Shamim Shakib
+      counts++;
 }
