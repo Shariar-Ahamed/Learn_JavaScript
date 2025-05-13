bad5427ce29866da62476cb42aef2d4a9cc76016
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-13 07:40:28 +0600

    Update

---
+++ Topic_29_While_Loop/While_Loop.js
@@ -0,0 +1,12 @@
+// while loop
+
+let count = 0;
+
+while (count < 5) {
+    console.log(" Hello World "); 
+    count++;
+    // count += 1;
+    // count = count + 1;
+    // count += 2;
+    // count = count + 2;
+}
