97ebb6720be3825095823acc6a83adbe50dc750b
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-13 08:18:59 +0600

    Update

--- topic_11_array/variable.js
+++ topic_11_array/variable.js
@@ -1,38 +1,37 @@
 // Array declaration and initialization
 var friendsAge = [15, 18, 22, 11, 30, 40];
-console.log(friendsAge);
+console.log(friendsAge); // [ 15, 18, 22, 11, 30, 40 ]
 
 var friendsName = ['Rakib', "Rifat", "Shamim", 'Farabi'];
-console.log(friendsName);
+console.log(friendsName); // [ 'Rakib', 'Rifat', 'Shamim', 'Farabi' ]
 
 
 
 // Type of variable
-console.log(typeof friendsName);
+console.log(typeof friendsName); // object
 
 
 
 // Find the length of the array
 var friendsAge = [15, 18, 22, 11, 30, 40];
-console.log(friendsAge.length)
+console.log(friendsAge.length) // 6
 
 var friendsName = ['Rakib', "Rifat", "Shamim", 'Farabi'];
-console.log(friendsName.length)
-
+console.log(friendsName.length) // 4
 
 
 // Find the Index of array
 var friendsAge = [15, 18, 22, 11, 30, 40];
-console.log(friendsAge.indexOf(30));
+console.log(friendsAge.indexOf(30)); // 4
 
 var friendsName = ['Rakib', "Rifat", "Shamim", 'Farabi'];
-console.log(friendsName.indexOf("Shamim"));
+console.log(friendsName.indexOf("Shamim")); // 2
 
 
 
 // Find the index position of an element in an array
 var friendsAge = [15, 18, 25, 11, 30, 40];
-console.log(friendsAge[2]);
+console.log(friendsAge[2]); // 25
 
 var indexElement = friendsAge[2];
-console.log(indexElement);
+console.log(indexElement); // 25
--- topic_12_array/variable.js
+++ topic_12_array/variable.js
@@ -1,31 +1,30 @@
 // Find the length of the array
 var number = [50, 80, 77, 45, 90, 100 ];
-console.log(number.length);
+console.log(number.length); // 6
 
 
 
 // Find the index position of an element in an array
 var number = [50, 80, 77, 45, 90, 100 ];
-console.log(number.indexOf(77));
+console.log(number.indexOf(77)); // 2
 
 var element = number.indexOf(77);
-console.log(element);
+console.log(element); // 2
 
 
 
 // Find the value of an element in an array
 var number = [50, 80, 77, 45, 90, 100 ];
-console.log(number[2]);
+console.log(number[2]); // 77
 
 var secndElement = number[2];
-console.log(secndElement);
+console.log(secndElement); // 77
 
 
 
 
 // Change the value of an element in an array
 var number = [50, 80, 77, 45, 90, 100 ];
-console.log(number);
-
+console.log(number); // [ 50, 80, 77, 45, 90, 100 ]
 number[0] = 33;
-console.log(number);
+console.log(number); // [ 33, 80, 77, 45, 90, 100 ]
