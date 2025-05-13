7d191270e3695180a5f435e485e31cfbca59fd46
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-13 08:00:57 +0600

    Update

--- Topic_29_While_Loop/While_Loop.js
+++ Topic_29_While_Loop/While_Loop.js
@@ -3,10 +3,22 @@
 let count = 0;
 
 while (count < 5) {
-    console.log(" Hello World "); 
+    console.log("Hello World : " + count); // Hello World : 0, Hello World : 1, Hello World : 2, Hello World : 3, Hello World : 4
+    console.log(count); // 0 1 2 3 4
+
     count++;
     // count += 1;
     // count = count + 1;
     // count += 2;
     // count = count + 2;
+}
+
+
+
+
+let i = 1;
+
+while (i <= 15) {
+    console.log(i); // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+    i++;
 }
---
+++ Topic_30_Understand_While_loop/Understand_While_loop.js
@@ -0,0 +1,8 @@
+// while loop
+
+let count = 10;
+
+while (count > 0) {
+    console.log(count); // 10 9 8 7 6 5 4 3 2 1
+    count--;
+}
