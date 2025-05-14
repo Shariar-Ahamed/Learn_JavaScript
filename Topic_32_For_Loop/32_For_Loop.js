c4239644b0a9a260e4689376d7a8a3c51c3a73d0
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-14 07:23:58 +0600

    Update

---
+++ Topic_32_For_Loop/32_For_Loop.js
@@ -0,0 +1,33 @@
+
+// for loop
+for ( let count = 10; count >= 0; count -- ){
+              console.log(count) // 10 9 8 7 6 5 4 3 2 1 0
+
+}
+
+
+// Print array elements
+const array = ["Shahriar", "Shuvo", "Shanto", "Shamim", "Shakib"];
+
+for ( let i = 0; i < array.length;  i ++ ){
+              console.log(array[i]) // Shahriar Shuvo Shanto Shamim Shakib
+}
+
+
+
+// Print array numbers
+const ary = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+for ( let x = 0; x < ary.length; x ++ ){
+                console.log(ary[x]) // 1 2 3 4 5 6 7 8 9 10
+}
+
+
+// Print 5 numbers
+for ( let y = 0; y <= 10; y++ ){
+                console.log(y);
+                if ( y === 5 ){
+                    console.log("Y is 5"); // Y is 5
+                    break; 
+                }
+}
