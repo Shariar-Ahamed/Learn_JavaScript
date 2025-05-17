3a388d4ac27e21c2212bd985670874901c8094d4
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-05-17 10:37:58 +0600

    Update

---
+++ Topic_35_Ternary_Operator/Ternary_Operator.js
@@ -0,0 +1,31 @@
+
+// Ternary Operator in JavaScript
+let age1 =15;
+let vote1
+
+if(age1 > 18){
+    vote1 = "You can vote";
+}
+else{
+    vote1 = "You cannot vote";
+}
+     console.log(vote1); // 'You cannot vote'
+
+
+
+
+
+let age = 18;
+
+let vote = (age >= 18) ? "You can vote" : "You cannot vote";
+
+     console.log(vote); // ''You can vote''
+
+
+
+
+let a = 200;
+let b;
+
+( a === 200 ) ? b = "True" : "False";
+     console.log(b); // 'True'
