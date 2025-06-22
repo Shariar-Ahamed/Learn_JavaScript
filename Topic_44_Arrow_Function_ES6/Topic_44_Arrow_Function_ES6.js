df3d78088b518a0263d2dc2b2b708e78f1bb4702
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-06-22 12:22:25 +0600

    Update

--- Topic_44_Arrow_Function_ES6/Topic_44_Arrow_Function_ES6.js
+++ Topic_44_Arrow_Function_ES6/Topic_44_Arrow_Function_ES6.js
@@ -1,15 +1,23 @@
+// Regular Function Syntax in JavaScript
+
 function sumNumbers(num1, num2) {
   return num1 + num2;
 }
 
 console.log(sumNumbers(15, 10)); // Output: 25  (Regular function syntax)
 
+//-----------
 
 
-
-
+// Arrow Function Syntax in JavaScript
 const sumNumbers1 = (num1, num2) => {
   return num1 + num2;
 }
 
 console.log(sumNumbers1(15, 10)); // Output: 25 (Arrow function syntax)
+
+//-----------
+
+
+
+// Arrow Function with Implicit Return
