acc0638495080fe44201675b64d09ef7c77a8789
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-06-22 12:18:57 +0600

    Update

---
+++ Topic_44_Arrow_Function_ES6/Topic_44_Arrow_Function_ES6.js
@@ -0,0 +1,15 @@
+function sumNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sumNumbers(15, 10)); // Output: 25  (Regular function syntax)
+
+
+
+
+
+const sumNumbers1 = (num1, num2) => {
+  return num1 + num2;
+}
+
+console.log(sumNumbers1(15, 10)); // Output: 25 (Arrow function syntax)
