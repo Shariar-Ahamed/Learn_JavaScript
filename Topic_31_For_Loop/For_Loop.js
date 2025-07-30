6ba88cf3eac517a3fd3f780caabcadde3f50393b
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-07-30 09:31:51 +0600

    Topic_31_For_Loop

--- Topic_31_For_Loop/For_Loop.js
+++ Topic_31_For_Loop/For_Loop.js
@@ -19,3 +19,22 @@ for ( let count = 0; count <= 10; count ++){
              console.log(count) // 0 1 2 3 4 5 6 7 8 9 10
 
 }
+
+
+// Ex : 
+
+
+
+// While loop 
+let num = 0;                    // Loop variable
+while ( num <5 ){              // loop condition : যতক্ষণ num < 5, ততক্ষণ loop চলবে
+        console.log(num);     // loop statement : বর্তমান num print হবে          
+        num++;               // loop veriable change : num বাড়বে ১ করে
+}
+// [Output: 0,1,2,3,4]
+
+
+// For loop
+for ( let num = 0 ; num <5 ; num++ ) {                  // for ( Loop variable ; loop condition ; loop veriable change )
+        console.log(num);       // 0,1,2,3,4           //loop statement
+}
