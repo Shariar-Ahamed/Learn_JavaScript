4fdf58a6b465cb617504dc7ac3eb25f1ef8f859f
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 07:33:28 +0600

    learn: practice basic JavaScript

--- Topic_21_Concatenation/Concatenation.js
+++ Topic_21_Concatenation/Concatenation.js
@@ -18,3 +18,9 @@ let number4 =  30;
 let result = number1 + number2 + number3 + number4;
 
 console.log(result); // '102030'
+
+
+console.log(20+"10") // '2010'
+console.log(20-"10") // '10'
+console.log(20*"10") // '200'
+console.log(20/"10") // '2'
