623cca51675842b5d75541bf056d146009df2982
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-02 16:25:00 +0600

    Arry Push Pop

--- topic_13_push_pop_array/variable.js
+++ topic_13_push_pop_array/variable.js
@@ -5,27 +5,27 @@ console.log(friendsName.indexOf('Aman'));
 
 
 // Push (Add to the end of the array)
-var friendsName = ["Bablu", 'Kalam', 'Aman', "Salam" ];
-console.log(friendsName);
+var friendsName = ["Bablu", 'Kalam', 'Aman', "Salam"];
+console.log(friendsName);  // ["Bablu", 'Kalam', 'Aman', "Salam"]
 
 friendsName.push('Shamim');
-console.log(friendsName);
+console.log(friendsName);  // ["Bablu", 'Kalam', 'Aman', "Salam", Shamim]
 
 
 
 // Push (Add to the end of the array)
 var numbers = [50, 80, 77, 45, 90, 100 ];
-console.log(numbers);
+console.log(numbers); // Array(6) [ 50, 80, 77, 45, 90, 100 ]
 
 numbers.push(500)
-console.log(numbers);
+console.log(numbers); // Array(7) [ 50, 80, 77, 45, 90, 100, 500 ]
 
 
 
 // Pop (Remove from the end of the array)
 var numbers = [50, 80, 77, 45, 90, 100 ];
-console.log(numbers);
+console.log(numbers); // Array(6) [ 50, 80, 77, 45, 90, 100 ]
 
 numbers.pop();
 numbers.pop();
-console.log(numbers);
+console.log(numbers); // [ 50, 80, 77, 45 ]
