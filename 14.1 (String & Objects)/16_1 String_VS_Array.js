8f67c2ac75cc78dff97424ddc0e554a3f671cde9
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-03 09:09:33 +0600

    String vs array

---
+++ 14.1 (String & Objects)/16_1 String_VS_Array.js
@@ -0,0 +1,22 @@
+// How many type declare Array
+const country = 'Bangladesh';
+const diviton = "Rajshahi";
+const district = `Dianjpur`;
+// const thana = new(`Rangpur`); 
+// console.log(thana);  Output : "Rangpur" is not a constructor
+
+
+
+// Total index
+const capital = 'Dha ka';
+console.log(capital.length); // 6
+
+// NO of index
+console.log(capital[4]);  // 'k'
+
+
+
+// String is immutable --> Not changeable
+// array is mutable --> Changeable
+capital[0] = "F"
+console.log(capital) // 'Dha ka'
