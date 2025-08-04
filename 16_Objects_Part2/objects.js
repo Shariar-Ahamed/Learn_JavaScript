88253974d4c1530e1e7baaee7bea8c57b2810168
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-04 10:29:04 +0600

    Objects properties and values

--- 16_Objects_Part2/objects.js
+++ 16_Objects_Part2/objects.js
@@ -13,36 +13,56 @@ var myObj = {
 };
 
 
-// Added a new property to myObj
+        //--------- Update Objct---------//
+//---------Added a new property to myObj---------
 myObj.friends = ["Rakib", "Rifat", "Shamim"];
 myObj.number = 55;
+myObj.age = 20;  // Previous Age Update
 
 
-// Update the name property of myObj
+//---------Update the name property of myObj---------
 myObj.name = "Shakib Khan";
 
 
-// delete age property from myObj
+//---------Delete age property from myObj---------
 delete myObj.age; 
 
 
 
 console.log(myObj);
-console.log(myObj.nextObj.favColor)
+/** OutPut: 
+{
+  name: 'Shakib Khan',
+  'full Name': 'Shariar Ahamed',
+  email: 'abc@yourdomain.com',
+  isSingle: true,
+  nextObj: { favMovies: 'Tom and Jerry', favColor: 'Blue' },
+  friends: [ 'Rakib', 'Rifat', 'Shamim' ],
+  number: 55
+}
+*/
+
+
+//-------------------------------------------------------//
+
+
+//---------Nested Output---------
+console.log(myObj.nextObj.favColor) // 'Blue'
 
 
 
-// Dot notation
+//---------Dot notation---------
 var x = myObj.name;
 console.log(x);                     // 'Shakib Khan'
+
 console.log(myObj.name);           // 'Shakib Khan'
 
 
 
-// Square bracket notation
+//---------Square bracket notation---------
 console.log(myObj["full Name"]); // 'Shariar Ahamed'
 
 
 
-// No Change property
+//---------No Change property---------
 Object.freeze(myObj); 
