379bdb6ccdc72a1512dbbaf78cacf832c175dae2
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-04 07:52:00 +0600

    Objects properties and values

---
+++ 14.1 (16 String & Objects)/16_5 Objects properties and values.js
@@ -0,0 +1,28 @@
+//--------Object--------//
+
+//----------Primitive-----------(basic, primary)
+const name = 'Shariar';
+const school = 'Daffodil School';
+const isPass = true;
+
+
+//----------Non Primitive-----------
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false,
+}
+
+
+//----------Non Primitive-----------
+const subject = {
+    name: 'biology',
+    teacher: 'Shariar Ahamed',
+    examDate: '30th Feb',
+    chapters: ['first', 'second', 'third'],
+    exam: {
+        name:'Fianl Exam',
+        marks: 100, 
+    }
+}
--- Test.js
+++ Test.js
@@ -1,6 +1,3 @@
 // It's just a test file to check if the code is working
 // and to see if the code is formatted correctly
 // and to see if the code is working
-
-
-
