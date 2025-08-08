e52f01142b3386c74e00625d273a39292b1d7ff8
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-08 22:44:22 +0600

    Traverse Array using for, while, and for-of loop in JavaScript

---
+++ 14.2 (18 More About Array , Traversal and Operation)/18_2 Traverse Array using for, while, and for-of loop in JavaScript.js
@@ -0,0 +1,19 @@
+// -------Traverse Array using for, while, and for-of loop in JavaScript-------
+
+let fruits = ['Apple', 'Banan', 'Daav'];
+
+// for loop 
+for( let i = 0; i < fruits.length; i++){
+    console.log(i)               // 0, 1, 2
+    console.log(fruits[i])      //'Apple' 'Banan' 'Daav'
+}
+
+
+// while loop 
+let i = 0;
+
+while ( i < fruits.length) {
+    console.log(i);              // 0, 1, 2
+    console.log(fruits[i]);     //'Apple' 'Banan' 'Daav'
+    i++;
+}
