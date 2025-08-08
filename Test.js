c099ca3fd628ca9c143da7d7ad65c8e8d4688833
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-08 19:43:30 +0600

    More About Array , Traversal and Operation

--- Test.js
+++ Test.js
@@ -2,3 +2,14 @@
 // and to see if the code is formatted correctly
 // and to see if the code is working
 
+
+
+let cash = [10, 20, 30, 40];
+let sum = 0;
+
+for ( let i = 0; i < cash.length; i++ ){
+    console.log(cash[i]);
+    sum = sum + cash[i];
+}
+
+console.log(sum);
