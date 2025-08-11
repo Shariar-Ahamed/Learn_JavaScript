056fe2fb1595fcd782881e014681acd1721675a0
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-11 06:56:00 +0600

    What are function and function syntax

---
+++ 37.1 (19 Fundamentals of Functions)/19-1 What are function and function syntax.js
@@ -0,0 +1,22 @@
+//* What are function and function syntax *//
+
+//------Function declar------
+function brushTeeth(){
+    console.log('pick up the brush'); //? 'pick up the brush'
+    console.log('add paste');  //? 'add paste'
+    console.log('2 min goso and goso and fena felo'); //? '2 min goso and goso and fena felo'
+}
+
+//------call the function-----
+brushTeeth();
+
+
+
+//todo---Math---
+function Number (num1, num2) {
+    sum = ( num1 + num2);
+    return sum;
+}
+
+Number(5,10);
+console.log(sum); //? 15
