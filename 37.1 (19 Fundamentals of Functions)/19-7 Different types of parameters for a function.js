1b0c3eba1de336a838e70cff7e8db89842f371cd
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-12 21:46:16 +0600

    evenSizeString

---
+++ 37.1 (19 Fundamentals of Functions)/19-7 Different types of parameters for a function.js
@@ -0,0 +1,35 @@
+//* Different types of parameters for a function *//
+//! for a given string tell me whether it has even number of characters or not
+
+//todo ------evenSizeString------
+function evenSizeString(str){
+    const size = str.length;
+    console.log(str, size);      //? [ 'Dhaka', 5 ]
+    if (size % 2 === 0 ){
+        console.log('Even Size');
+        return true;
+    }
+    else {
+        console.log('Odd Size')     //? 'Odd Size'
+        return false;
+    }
+}
+
+evenSizeString('Dhaka');
+
+
+
+
+//todo ------evenSizeString------
+function doubleOrTriple(number, doDouble){
+    if ( doDouble === true ){
+        const result = number * 2;
+        return result;
+    }
+    else {
+        const result = number * 3;
+         return result;
+    }
+}
+
+console.log(doubleOrTriple(5, true)); //? 10
