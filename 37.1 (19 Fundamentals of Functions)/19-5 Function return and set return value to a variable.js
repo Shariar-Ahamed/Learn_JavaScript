3225d1a9e87eb6c1eb919c4134d15eb7e631fafd
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-12 20:59:10 +0600

    Function return and set return value to a variable

---
+++ 37.1 (19 Fundamentals of Functions)/19-5 Function return and set return value to a variable.js
@@ -0,0 +1,9 @@
+//* Function return and set return value to a variable *//
+
+function TenTimes (number) {
+    const result = number * 10;
+    return result;
+}
+
+const output = TenTimes(5);  // Using Return extra varuable
+console.log('Output', output)  //? [ 'Output', 50 ]
