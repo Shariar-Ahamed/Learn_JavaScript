ed88162415296b2b76b1dc931fb48b14a69ff557
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-15 07:06:27 +0600

    Kilometre to mile convert

--- Milestone 4 Problem Solving With JavaScript/20-2 Unit Convert Inch to Feet, miles to kilometre.js
+++ Milestone 4 Problem Solving With JavaScript/20-2 Unit Convert Inch to Feet, miles to kilometre.js
@@ -24,4 +24,15 @@ function inchToFeet2(inch){
 }
 
 const RiponHeight = inchToFeet2(75);
-console.log(RiponHeight)      //? '6 Feet 3 Inch '
+console.log(RiponHeight)      //? '6 Feet 3 Inch'
+
+
+
+//Todo:------Kilometre to mile convert------
+function kilometreToMile(kilometre){
+    const mile = kilometre * 0.621371;
+    return mile;
+}
+
+const inputKilometre = kilometreToMile(10);
+console.log(inputKilometre);    //? 6.21371
