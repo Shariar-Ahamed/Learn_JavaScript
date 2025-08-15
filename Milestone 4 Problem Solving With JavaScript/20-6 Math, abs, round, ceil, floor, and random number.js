596f4e80231d2ae3b90d86ea0c19efd32ddabf1a
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-15 09:32:11 +0600

    Math Always Positive

---
+++ Milestone 4 Problem Solving With JavaScript/20-6 Math, abs, round, ceil, floor, and random number.js
@@ -0,0 +1,16 @@
+//* Math, abs, round, ceil, floor, and random number */ 
+
+//Todo: ------Math min,max------
+const min = Math.min(50, 20, 1, 25, 125, -100);
+const max = Math.max(50, 20, 1, 25, 125, -100);
+
+console.log(min); //? -100
+console.log(max); //? 125
+
+//Todo: ------Math PI------
+console.log(Math.PI) //? 3.141592653589793
+
+//Todo: ------Math Always Positive------
+console.log(Math.abs(-100)) //? 100
+console.log(Math.abs(100)) //? 100
+
