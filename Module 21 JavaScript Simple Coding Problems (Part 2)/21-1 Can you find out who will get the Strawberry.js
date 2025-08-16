c5c89a6b89af11a85b41d838cfb45d57a819c58c
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-16 08:11:00 +0600

    Three team

--- Module 21 JavaScript Simple Coding Problems (Part 2)/21-1 Can you find out who will get the Strawberry.js
+++ Module 21 JavaScript Simple Coding Problems (Part 2)/21-1 Can you find out who will get the Strawberry.js
@@ -9,7 +9,7 @@ if(Disa > Salman) {
     console.log("Disha will be get the Strawberyy");
 }
 else {
-    console.log("Salman will be get the Strawberyy");
+    console.log("Salman will be get the Strawberyy");  //? 'Salman will be get the Strawberyy'
 }
 
 
@@ -29,5 +29,42 @@ const max1 = getMax(96, 79);
 const max2 = getMax(100, 59);
 const ultimateMax = getMax(max1, max2);
 
-console.log("max of two is: ", ultimateMax);
+console.log("max of two is: ", ultimateMax);  //? [ 'max of two is: ', 100 ]
 
+
+
+
+//Todo:-------Three team-------
+const jim = 56;
+const tim = 89;
+const kim = 168;
+
+if (jim > tim && jim > kim) {
+    console.log("jim is the ultimate boss");
+}
+else if ( tim > jim & tim > kim ) {
+    console.log("tim is the ultimate boss");
+}
+else {
+    console.log("kim is the ultimate boss");  //? 'kim is the ultimate boss'
+}
+
+
+//* Using funcation:
+function maxOfThree (num1, num2, num3) {
+    if (num1 > num2 && num1 > num3){
+        console.log("jim is the ultimate boss: ");
+        return num1;
+    }
+    else if( num2 > num1 && num2 > num3) {
+        console.log("tim is the ultimate boss: ");
+        return num2;
+    }
+    else {
+        console.log("kim is the ultimate boss: ");  //? 'kim is the ultimate boss: '
+        return num3;
+    }
+}
+
+const finalResult = maxOfThree(56, 89, 168);
+console.log(finalResult)  //? 168
