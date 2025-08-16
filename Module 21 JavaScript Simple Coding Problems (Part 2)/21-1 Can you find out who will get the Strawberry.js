463f82475a947f6eafde459ddeadef4edc906548
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-16 07:32:53 +0600

    Using a funcation

--- Module 21 JavaScript Simple Coding Problems (Part 2)/21-1 Can you find out who will get the Strawberry.js
+++ Module 21 JavaScript Simple Coding Problems (Part 2)/21-1 Can you find out who will get the Strawberry.js
@@ -12,3 +12,22 @@ else {
     console.log("Salman will be get the Strawberyy");
 }
 
+
+
+
+//Todo:------Using a funcation------
+function getMax(num1, num2) {
+    if (num1 > num2){
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
+
+const max1 = getMax(96, 79);
+const max2 = getMax(100, 59);
+const ultimateMax = getMax(max1, max2);
+
+console.log("max of two is: ", ultimateMax);
+
