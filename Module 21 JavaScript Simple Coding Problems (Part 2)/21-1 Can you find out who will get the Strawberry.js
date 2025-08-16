a4f56dd1ffc0205f6ce0b8399c447b95e26716a9
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-16 07:32:23 +0600

    Using if else

---
+++ Module 21 JavaScript Simple Coding Problems (Part 2)/21-1 Can you find out who will get the Strawberry.js
@@ -0,0 +1,14 @@
+//* Can you find out who will get the Strawberry? */
+
+//Todo: Max
+//Todo:------Using if else------
+const Disa = 56;
+const Salman = 95;
+
+if(Disa > Salman) {
+    console.log("Disha will be get the Strawberyy");
+}
+else {
+    console.log("Salman will be get the Strawberyy");
+}
+
