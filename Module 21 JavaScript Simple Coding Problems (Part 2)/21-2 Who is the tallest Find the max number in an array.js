36ba864d794e6a0a3b407d09ab95086451057de3
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-16 09:41:57 +0600

    Who is the tallest? Find the max number in an array

---
+++ Module 21 JavaScript Simple Coding Problems (Part 2)/21-2 Who is the tallest Find the max number in an array.js
@@ -0,0 +1,19 @@
+//* Who is the tallest? Find the max number in an array */
+
+
+//Todo:-------Max search------
+function getMax(numbers){
+    let updateMax = numbers[0];
+    for(const num of numbers){
+        if(num > updateMax){
+          updateMax = num;
+        }          
+    }
+    return updateMax;
+}
+
+
+const heights = [65, 66, 68, 72, 78, 60, 65, 66];
+
+const max = getMax(heights);
+console.log(max);      //? 78
